27f01d18795822f870d08dfcb6fbdb458b31b6dc
rabbit <barateligeorge@yandex.ru>
2022-09-18 17:34:17 +0300

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import webpack from 'webpack';
+import {buildWebpackConfig} from './config/build/buildWebpackConfig';
+import {BuildEnv, BuildMode, BuildPaths} from './config/build/types/config';
+
+export default (env: BuildEnv): webpack.Configuration => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+    const mode: BuildMode = env.mode || 'development';
+    const port = env.port || 3000;
+    const isDev = mode === 'development';
+
+    return buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port,
+    });
+}
+
