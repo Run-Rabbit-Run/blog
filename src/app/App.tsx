6065fc59e24ff0e2b54db1768153f18874292a6a
rabbit <barateligeorge@yandex.ru>
2022-09-21 20:29:12 +0300

    Svg and file loaders. Added Button and ThemeSwitcher components.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,12 @@ import {Navbar} from 'widgets/Navbar';
 import './styles/index.scss'
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>Toggle Theme</button>
         </div>
     );
 };
