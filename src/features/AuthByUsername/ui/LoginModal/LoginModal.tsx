7fe333c225655275761484d65bc5b12caed239d2
rabbit <barateligeorge@yandex.ru>
2023-02-05 17:06:03 +0300

    32 fix eslint warnings

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,5 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string;
@@ -20,7 +18,7 @@ export const LoginModal = (props: LoginModalProps) => {
         <Modal
             isOpen={isOpen}
             onClose={onClose}
-            className={classNames(cls.loginModal, {}, [className])}
+            className={className}
             lazy
         >
             <LoginForm />
